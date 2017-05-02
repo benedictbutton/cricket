a35207c744843e706cce32cccfe339711a37174a
benedictbutton <benedictdaly@hotmail.com>
2017-05-02 18:22:35 -0400

    unresolved merge conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true
-<<<<<<< HEAD
-=======
 
   has_one :player
->>>>>>> 2fe611d9b7dfd036e8bea75008ae3dbc8412406b
 end
