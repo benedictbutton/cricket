08dfb16c12c374b2c364140385b9df027f80b60c
benedictbutton <benedictdaly@hotmail.com>
2017-05-02 10:10:08 -0400

    authentication

---
+++ db/migrate/20170501220513_add_users_columns.rb
@@ -0,0 +1,15 @@
+class AddUsersColumns < ActiveRecord::Migration[5.1]
+  def up
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :username, :string, null: false
+    add_column :users, :avatar, :string
+  end
+
+  def down
+    remove_column :users, :first_name
+    remove_column :users, :last_name
+    remove_column :users, :username
+    remove_column :users, :avatar
+  end
+end
