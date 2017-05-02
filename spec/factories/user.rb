f60770e4840368d637057eeacfb0e545d51ac2f5
benedictbutton <benedictdaly@hotmail.com>
2017-05-02 11:05:40 -0400

    Log Out

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :user do
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    avatar Faker::Avatar.image
+    password Faker::Internet.password
     sequence :email do |n|
       "#{Faker::Internet.email}#{n}"
     end
-    password Faker::Internet.password
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
     sequence :username do |n|
       "#{Faker::Internet.user_name}#{n}"
     end
-    avatar Faker::Avatar.image
   end
 end
