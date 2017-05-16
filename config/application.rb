adba79de0388937333dac73e7c63c903146ad17b
benedictbutton <benedictdaly@hotmail.com>
2017-05-16 03:40:10 -0400

    make it better

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Heroku error
-config.assets.initialize_on_precompile = false
-
 module Cricket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
