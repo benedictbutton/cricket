ac59d044aa33dca608a52ee9677c8437b2e0caf8
benedictbutton <benedictdaly@hotmail.com>
2017-05-16 03:03:23 -0400

    precompile false

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,12 @@ require_relative 'boot'
 
 
 require 'rails/all'
-require 'devise'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+# Heroku error
+config.assets.initialize_on_precompile = false
 
 module Cricket
   class Application < Rails::Application
