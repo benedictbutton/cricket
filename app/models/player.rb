35ce018da0a6bf66bc19aa509e9ee7586a599b6a
benedictbutton <benedictdaly@hotmail.com>
2017-09-18 19:02:56 -0400

    player custom uniqueness validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,22 @@
 class Player < ApplicationRecord
+  validate :player_names, on: :create
   validates :name, presence: true
 
   has_many :scores
   has_many :games, through: :scores
+
+  def player_names
+    player_names = []
+    id = Game.last[:user_id]
+     games = Game.where(user_id: id)
+     games.each do |game|
+       game.players.select(:name, :id).distinct.each do |player|
+         player_names << player.name
+       end
+     end
+     players = player_names.uniq
+     if players.include?(name)
+       errors.add(:name, "Player name '#{name}' has already been taken")
+     end
+  end
 end
